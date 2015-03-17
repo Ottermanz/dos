22bcd0f87e3e5eefe88f7407a70e551d239944c7
Ottermanz <ottermanz1998@gmail.com>
2015-03-16 19:14:36 -0700

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,4 +1,5 @@
 @echo off
+title Dos Emporium V1
 :mainmenu
 color 2
 cls
@@ -9,7 +10,8 @@ echo 1. Creations
 echo 2. Creators
 echo 3. Versions
 echo 4. References
-echo 5. Quit
+echo 5. Options
+echo 6. Quit
 echo.
 echo.
 set /p mainmenu=
@@ -17,9 +19,11 @@ if %mainmenu%==1 goto creations1
 if %mainmenu%==2 goto creators1
 if %mainmenu%==3 goto versions1
 if %mainmenu%==4 goto references1
-if %mainmenu%==5 goto quit
+if %mainmenu%==5 goto options
+if %mainmenu%==6 goto quit
 
 :creations1
+::THIS IS THE PLACE WHERE ALL THE CREATIONS ARE!!!
 cls
 echo Creations - Page 1
 echo.
@@ -29,9 +33,14 @@ echo 0. Back
 echo.
 echo.
 set /p creations1=Input:
-if %creations1%==1 goto hackingsimulator2015
+if %creations1%==1 call hackingsimulator2015.bat
+if %creations1%==0 goto mainmenu
 
 :creators1
+::These are the contributors
+::They made stuff
+::It should go like this:
+::(Contributor) - (#) Contribution(s)
 cls
 echo Creators Room
 echo The room with all the creators and the dumb stuff they made.
@@ -42,10 +51,17 @@ echo 0. Back
 echo.
 echo.
 set /p creators1=Input:
-if %creators1%==1 goto C_Otterman
+if %creators1%==1 (
+    cls
+    type C_Otterman.txt
+    pause>nul
+    goto creators1
+)
 if %creators1%==0 goto mainmenu
 
 :versions1
+::This is the version history.
+::When the emporium expands, there will be multiple versions.
 cls
 echo Version History
 echo All the changes that were made in the emporium
@@ -58,11 +74,27 @@ if %versions1%==1 goto V_1
 if %versions1%==0 goto mainmenu
 
 :references1
+::References are links to where the program came from
+::It will be like this:
+::(Creation) - (Creator): (url)
+::Or something like that
 cls
 echo Still In Development
 echo.
 echo.
-pause
+pause>nul
+goto mainmenu
+
+:options
+::These will include the following options:
+::Text Color
+::Backround Color
+::Just for fun :)
+cls
+echo Still In Development
+echo.
+echo.
+pause>nul
 goto mainmenu
 
 :quit
@@ -70,5 +102,5 @@ cls
 echo Goodbye then :/
 echo.
 echo.
-pause
+sleep 1>nul
 quit
