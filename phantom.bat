663dfbd35063996955ace8e51540cb56aa2b2ecd
Ottermanz <ottermanz1998@gmail.com>
2015-04-16 16:51:05 -0700

    Update phantom.bat
    echo... echo... echo...

--- phantom.bat
+++ phantom.bat
@@ -78,35 +78,43 @@ if %pass2%==epic dream worlds (
 ) else (
         echo INCORRECT PASSWORD
 )
->> Contacting Steam permissions authority
->> Loading assignment 'phase 2' matrix
->> Enabling access pattern
->> Flushing to storage
->> Assignment unlocked
->> Unlock phase 2 completed.
-
-Unlock phase 3 initiated...
-
-dracut: rd_NO_MD: removing MD RAID activation 
-ata_piix 0000:00:01.1: version 2.14a 
-ata_piix 0000:00:01.1: setting latency timer to 128 
-scsi0 : ata_piix 
-scsi1 : ata_piix 
-ata1: PATA max MWDMA2 cmd 0x1e0 ctl 0x346 bmdma 0xc003 irq 12 
-ata1.00: ATA-6: QEMU HARDDISK, 0.11.2, max UDMA/50 
-ata1.00: 3540407146 sectors, multi 10: SBN 
-ata1.00: configured for MWDMA2 
-scsi 0:0:0:0: Direct-Access ATA QEMU HARDDISK 0.11 PQ: 0 ANSI: 6 
-virtio-pci 0000:00:06.0: PCI INT A -> CHI: 7.25154120937236 -> DIG 11 (max, floor) -> IRQ 13
-
-ENTER PASSCODE> *********
-ACCESS GRANTED
-
->> Contacting Steam permissions authority
->> Loading assignment 'phase 3' matrix
->> Enabling access pattern
->> Flushing to storage
->> Assignment unlocked
->> Unlock phase 3 completed
-
-Please wait for further instructions...
+:phase2pass
+echo // Contacting Steam permissions authority
+echo // Loading assignment 'phase 2' matrix
+echo // Enabling access pattern
+echo // Flushing to storage
+echo // Assignment unlocked
+echo // Unlock phase 2 completed.
+echo. 
+echo Unlock phase 3 initiated...
+echo.
+echo dracut: rd_NO_MD: removing MD RAID activation 
+echo ata_piix 0000:00:01.1: version 2.14a 
+echo ata_piix 0000:00:01.1: setting latency timer to 128 
+echo scsi0 : ata_piix 
+echo scsi1 : ata_piix 
+echo ata1: PATA max MWDMA2 cmd 0x1e0 ctl 0x346 bmdma 0xc003 irq 12 
+echo ata1.00: ATA-6: QEMU HARDDISK, 0.11.2, max UDMA/50 
+echo ata1.00: 3540407146 sectors, multi 10: SBN 
+echo ata1.00: configured for MWDMA2 
+echo scsi 0:0:0:0: Direct-Access ATA QEMU HARDDISK 0.11 PQ: 0 ANSI: 6 
+echo virtio-pci 0000:00:06.0: PCI INT A -) CHI: 7.25154120937236 -) DIG 11 (max, floor) -) IRQ 13
+echo.
+set /p pass3=ENTER PASSCODE:
+if %pass3%== (
+        echo ACCESS GRANTED
+        goto phase3pass
+) else (
+        echo INCORRECT PASSCODE
+)
+:phase3pass
+echo // Contacting Steam permissions authority
+echo // Loading assignment 'phase 3' matrix
+echo // Enabling access pattern
+echo // Flushing to storage
+echo // Assignment unlocked
+echo // Unlock phase 3 completed
+echo.
+echo Please wait for further instructions...
+echo.
+pause > nul
